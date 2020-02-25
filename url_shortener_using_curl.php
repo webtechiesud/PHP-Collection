0fdb111cd824b4aea4fd995129b960cb135959d5
Sudhanshu Singh <45901941+webtechiesud@users.noreply.github.com>
2020-02-25 11:46:05 +0530

    url_shortener_using_curl.php

---
+++ url_shortener_using_curl.php
@@ -0,0 +1,23 @@
+<?php 
+
+//URL SHORTENER
+	function get_url($url)  
+{  
+	$ch = curl_init();  
+	$timeout = 5;  
+	curl_setopt($ch,CURLOPT_URL,'http://tinyurl.com/api-create.php?url='.$url);  
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);  
+	curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);  
+	$data = curl_exec($ch);  
+	curl_close($ch);  
+	return $data;  
+}
+
+
+
+$url='https://www.amazon.com/Kindle-Wireless-Reading-Display-Globally/dp/B003FSUDM4/ref=amb_link_353259562_2?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=center-10&pf_rd_r=11EYKTN682A79T370AM3&pf_rd_t=201&pf_rd_p=1270985982&pf_rd_i=B002Y27P3M';
+
+$shorturl = get_url($url);
+echo $shorturl;
+
+?>
